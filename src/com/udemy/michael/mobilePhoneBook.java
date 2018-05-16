8cf29233de42784f9232cc92dd496244e539d6b6
M <M@Office_Computer>
2018-05-15 22:54:32 -0400

    Mobile Phone changes

---
+++ src/com/udemy/michael/mobilePhoneBook.java
@@ -0,0 +1,31 @@
+package com.udemy.michael;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+
+public class mobilePhoneBook {
+	private static Scanner scan = new Scanner(System.in);
+	
+	ArrayList<String> names = new ArrayList<String>();
+	
+	ArrayList<Integer> phoneNumber =  new ArrayList<Integer>();
+	
+	
+	public static void saveNames(String Name) {
+		System.out.println("Please Enter First & Last Name to save \n");
+		Name.equals(scan.nextLine());
+		
+		if (!Name.isEmpty()) {
+			System.out.println("The name " + Name + "has been saved");
+		}
+		System.out.println("No name has been entered");
+		
+	}
+	
+	
+	
+	
+
+}
